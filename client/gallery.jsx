e7e52377cf63401405fa9903d04b30c2631efbf5
Gaurav Gulati <ggulati3@gmail.com>
2018-07-18 13:15:12 -0700

    Basic Setup For Gallery Feature set up

---
+++ client/gallery.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedElement: null
+    }
+  }
+  render () {
+    return (<p> this should render once stuff is configured correctly </p>);
+  }
+}
+
+window.Gallery = Gallery;
+console.log(module.exports);

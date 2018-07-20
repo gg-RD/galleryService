b11ee4d812b8e7e176069ad58ea19d824818cef0
Gaurav Gulati <ggulati3@gmail.com>
2018-07-19 19:46:41 -0700

    Added Grid Component

---
+++ client/Components/grid.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+class Grid extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div>this is the grid</div>
+		);
+	}
+}
+
+export default Grid;
--- client/gallery.jsx
+++ client/gallery.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Grid from './Components/grid.jsx';
 
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedElement: null
+      name: 'hi'
     }
+    this.shoe = 'null';
+    this.image = [];
+
     this.getOriginalData = this.getOriginalData.bind(this);
   }
 
   getOriginalData() {
   	axios.get('/AirJordan1').then( (response) => {
-  		console.log(JSON.stringify(response['data']));
+      console.log(response.data)
+      this.setState({name: response.data.name})
   	}).catch( (error) => {
   		console.log(error);
   	})
   }
+  componentDidMount(){
+    this.getOriginalData();
+  }
 
   render () {
     return (
-    	<div>{this.getOriginalData()}</div>
+    	<div id = 'grid'>{this.state.name}
+        <Grid /> 
+      
+      </div>
+
     );
   }
 }
